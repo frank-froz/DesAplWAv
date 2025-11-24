257078948512ebabfaab1e7ea202466ffb8f44e6
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-11-23 20:04:48 -0500

    feat: configurar layout principal, SEO (robots.txt, sitemap) y agregar imagen de perfil

---
+++ semana14/my-portfolio/src/app/robots.ts
@@ -0,0 +1,20 @@
+import { personalInfo } from '@/lib/data';
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/', '/_next/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/', '/admin/'],
+      },
+    ],
+    sitemap: `${personalInfo.siteUrl}/sitemap.xml`,
+  };
+}
